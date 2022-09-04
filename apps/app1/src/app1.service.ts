24a83b5e3be9e8c9b08a8285a5757a20f90644d3
Cristian Blazquez Bustos <sitex696@gmail.com>
2022-09-04 14:29:17 +0200

    Create dockerfiles for all apps

--- apps/app1/src/app1.service.ts
+++ apps/app1/src/app1.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class App1Service {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World from APP1!';
   }
 }
