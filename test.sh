0262a33c18803b9b9e5254e6c8458beed26f05b4
Cristian Blazquez Bustos <sitex696@gmail.com>
2022-09-05 17:26:07 +0200

    Test automatic deploy changed apps

--- test.sh
+++ test.sh
@@ -12,4 +12,4 @@ apps=("apps/app1/" "apps/app2/src" "apps/app2/test" "apps/app1")
 for path in ${apps[@]}; do
   app=$(echo "$path" | sed -e 's/apps\///g' | sed -e 's/\/.*//g')
   echo "$app"
-done | jq -R -s 'split("\n") -[""]' | jq -c '{appName: [.|unique]}'
+done | jq -R -s 'split("\n") -[""]' | jq -c '{appName: .|unique}'
