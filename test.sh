276521495ed545780bac7748ae11f1fad0372409
Cristian Blazquez Bustos <sitex696@gmail.com>
2022-09-05 13:12:44 +0200

    test changed files

--- test.sh
+++ test.sh
@@ -4,6 +4,12 @@
 #
 #echo $outputJson;
 
-test=apps/app2/src
-app=$(echo "$test" | sed -e 's/apps\///g' | sed -e 's/\/.*//g')
-echo $app
+#test=apps/app2/src
+#app=$(echo "$test" | sed -e 's/apps\///g' | sed -e 's/\/.*//g')
+#echo $app
+
+apps=("apps/app1/" "apps/app2/src" "apps/app2/test" "apps/app1")
+for path in ${apps[@]}; do
+  app=$(echo "$path" | sed -e 's/apps\///g' | sed -e 's/\/.*//g')
+  echo "$app"
+done | jq -R -s 'split("\n") -[""]' | jq -c '{appName: [.|unique]}'
