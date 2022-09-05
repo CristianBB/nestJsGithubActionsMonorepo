f64fea8f1b5b177429b2e90d4f47c91d42fc5cb9
Cristian Blazquez Bustos <sitex696@gmail.com>
2022-09-05 09:42:14 +0200

    Test matrix from json generated

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 outputJson=$(for app in $(find apps/* -type d -maxdepth 0 -exec basename {} \;); do
   echo "$app"
-done | jq -R -s 'split("\n") -[""]' | jq '{appName: .}')
+done | jq -R -s 'split("\n") -[""]' | jq -c '{appName: .}')
 
 echo $outputJson;
